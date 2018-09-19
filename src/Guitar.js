009e28bc67c68acb46d98a47acc639beefcc3d3d
Uday Trivedi <trivediu@gmail.com>
2018-09-18 18:56:15 -0700

    Add Src Folder With Index.html, App.js, & Guitar.js

---
+++ src/Guitar.js
@@ -0,0 +1,8 @@
+export const Guitar = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.make),
+    React.createElement("h2", {}, props.model),
+  ]);
+};
+
+export default Guitar;
